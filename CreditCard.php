832bbd3619fdf96d7143afe097a43159f614b563
andrea <git@andreaemmanuele.it>
2021-05-18 10:15:36 +0200

    Getter & Setter CreditCard

--- CreditCard.php
+++ CreditCard.php
@@ -14,4 +14,54 @@ class CreditCard
         $this->holder = $holder;
         $this->circuit = $circuit;
     }
+
+    public function getPan()
+    {
+        return $this->pan;
+    }
+
+    public function getCvv()
+    {
+        return $this->cvv;
+    }
+
+    public function getExpirationDate()
+    {
+        return $this->expiration_date;
+    }
+
+    public function getHolder()
+    {
+        return $this->holder;
+    }
+
+    public function getCircuit()
+    {
+        return $this->circuit;
+    }
+
+    public function setPan($pan)
+    {
+        $this->pan = $pan;
+    }
+
+    public function setCvv($cvv)
+    {
+        $this->cvv = $cvv;
+    }
+
+    public function setExpirationDate($expiration_date)
+    {
+        $this->expiration_date = $expiration_date;
+    }
+
+    public function setHolder($holder)
+    {
+        $this->holder = $holder;
+    }
+
+    public function setCircuit($circuit)
+    {
+        $this->circuit = $circuit;
+    }
 }
