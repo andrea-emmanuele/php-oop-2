ca6a21a0495d6019cbccd33f9c07ebfd22304879
andrea <git@andreaemmanuele.it>
2021-05-18 10:12:39 +0200

    Getter & Setter User

--- User.php
+++ User.php
@@ -20,6 +20,76 @@ class User
         $this->$password = $password;
     }
 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    public function getDateOfBirth()
+    {
+        return $this->date_of_birth;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getCreditCards()
+    {
+        return $this->creditCards;
+    }
+
+    public function getCart()
+    {
+        return $this->cart;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    public function setDateOfBirth($date_of_birth)
+    {
+        $this->date_of_birth = $date_of_birth;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
     public function setCreditCards($creditCard) {
         array_push($this->cart, $creditCard);
     }
--- index.php
+++ index.php
@@ -8,4 +8,5 @@ $userRan = new User(01, 'Pippo', 'Germano', '12/06/1998', 'pippo81', 'securePass
 $bike = new Product(01, 'mountainBike', 'something here', 'available');
 $cardRan = new CreditCard(5354662178382, 637, '12/28', 'Pippo Rossi', 'Mastercard');
 
-
+$userRan->setCreditCards($cardRan);
+$userRan->addToCart($bike, 1);
